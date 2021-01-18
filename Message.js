0b9387ee4722f80b74decc96b54614575d993408
Rainbrand <26818172+Rainbrand@users.noreply.github.com>
2021-01-18 17:51:16 +0300

    Delete Message.js

--- Message.js
+++
@@ -1,8 +0,0 @@
-class Message{
-    constructor(text, roomName, sender="system") {
-        this.time = new Date().toUTCString()
-        this.text = text
-        this.sender = sender
-        this.roomName = roomName
-    }
-}
